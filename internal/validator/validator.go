94cc71e852425c40450ad901cc5d4f1c38c88d65
Hilgard <hilgardvr@protonmail.com>
2025-07-15 10:03:33 +0200

    user signup 10.3

--- internal/validator/validator.go
+++ internal/validator/validator.go
@@ -1,13 +1,36 @@
 package validator
 
 import (
+	"regexp"
 	"slices"
 	"strings"
 	"unicode/utf8"
 )
 
+var EmailRX = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
+
+func MinChars(value string, n int) bool {
+	return utf8.RuneCountInString(value) >= n
+}
+
+func Matches(value string, rx *regexp.Regexp) bool {
+	return rx.MatchString(value)
+}
+
 type Validator struct {
 	FieldErrors map[string]string
+}
+
+func NotBlank(value string) bool {
+	return strings.TrimSpace(value) != ""
+}
+
+func MaxChars(value string, n int) bool {
+	return utf8.RuneCountInString(value) <= n
+}
+
+func PermittedValue[T comparable](value T, permittedValues ...T) bool {
+	return slices.Contains(permittedValues, value)
 }
 
 func (v *Validator) Valid() bool {
@@ -28,16 +51,4 @@ func (v *Validator) CheckField(ok bool, key, message string) {
 	if !ok {
 		v.AddFieldError(key, message)
 	}
-}
-
-func NotBlank(value string) bool {
-	return strings.TrimSpace(value) != ""
-}
-
-func MaxChars(value string, n int) bool {
-	return utf8.RuneCountInString(value) <= n
-}
-
-func PermittedValue[T comparable](value T, permittedValues ...T) bool {
-	return slices.Contains(permittedValues, value)
 }

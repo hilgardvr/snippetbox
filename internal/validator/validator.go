d47ecdcf967c089668cb0d8a1820642deaf6d1cc
Hilgard <hilgardvr@protonmail.com>
2025-07-15 19:39:52 +0200

    user login 10.4

--- internal/validator/validator.go
+++ internal/validator/validator.go
@@ -7,34 +7,19 @@ import (
 	"unicode/utf8"
 )
 
+type Validator struct {
+	NonFieldErrors []string
+	FieldErrors    map[string]string
+}
+
 var EmailRX = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
 
-func MinChars(value string, n int) bool {
-	return utf8.RuneCountInString(value) >= n
-}
-
-func Matches(value string, rx *regexp.Regexp) bool {
-	return rx.MatchString(value)
-}
-
-type Validator struct {
-	FieldErrors map[string]string
-}
-
-func NotBlank(value string) bool {
-	return strings.TrimSpace(value) != ""
-}
-
-func MaxChars(value string, n int) bool {
-	return utf8.RuneCountInString(value) <= n
-}
-
-func PermittedValue[T comparable](value T, permittedValues ...T) bool {
-	return slices.Contains(permittedValues, value)
-}
-
 func (v *Validator) Valid() bool {
-	return len(v.FieldErrors) == 0
+	return len(v.FieldErrors) == 0 && len(v.NonFieldErrors) == 0
+}
+
+func (v *Validator) AddNonFieldError(message string) {
+	v.NonFieldErrors = append(v.NonFieldErrors, message)
 }
 
 func (v *Validator) AddFieldError(key, message string) {
@@ -51,4 +36,24 @@ func (v *Validator) CheckField(ok bool, key, message string) {
 	if !ok {
 		v.AddFieldError(key, message)
 	}
+}
+
+func MinChars(value string, n int) bool {
+	return utf8.RuneCountInString(value) >= n
+}
+
+func Matches(value string, rx *regexp.Regexp) bool {
+	return rx.MatchString(value)
+}
+
+func NotBlank(value string) bool {
+	return strings.TrimSpace(value) != ""
+}
+
+func MaxChars(value string, n int) bool {
+	return utf8.RuneCountInString(value) <= n
+}
+
+func PermittedValue[T comparable](value T, permittedValues ...T) bool {
+	return slices.Contains(permittedValues, value)
 }

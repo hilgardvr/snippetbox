fe40777f865a6ff61ebce8e0a0afee9c7c4d6c31
Hilgard <hilgardvr@protonmail.com>
2025-07-16 17:00:37 +0200

    handler middleware testing 13.2

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -30,6 +30,10 @@ type userLoginForm struct {
 	validator.Validator `form:"-"`
 }
 
+func ping(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("OK"))
+}
+
 func (app *application) userLoginGet(w http.ResponseWriter, r *http.Request) {
 	data := app.newTemplateData(r)
 	data.Form = userLoginForm{}
